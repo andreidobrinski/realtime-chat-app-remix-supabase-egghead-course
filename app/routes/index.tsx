d56abb5e482c0ac6d9b4095c33fe369866b025c4
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-01-09 15:24:22 -0500

    get messages from supabase

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,18 @@
+import { useLoaderData } from '@remix-run/react';
+import supabase from 'utils/supabase';
+
+export const loader = async () => {
+  const { data } = await supabase.from('messages').select();
+
+  return { data };
+}
+
 export default function Index() {
+  const { data } = useLoaderData();
+
   return (
-    <h1>hello</h1>
+    <pre>
+      {JSON.stringify(data, null, 2)}
+    </pre>
   );
 }
